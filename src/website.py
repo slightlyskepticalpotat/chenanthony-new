489de655c457b5e7ca4d8fad42a82c14edf1470d
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2022-08-08 11:38:09 -0400

    intiial testing

--- src/website.py
+++ src/website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/pgp")
+def pgp():
+    with app.open_resource("static/pgp.txt") as file:
+        pgp_key = file.read().decode("utf-8")
+    return render_template("default.html", title = "PGP Key", text = pgp_key)
+    
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
